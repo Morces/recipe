fcf8ca7e20087f650a52231f10c8e8a149b7ef6b
MOSES KARANI <karanim594@gmail.com>
2023-11-18 18:34:38 +0300

    set up folder structure

---
+++ src/navigation/index.js
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/Home";
+
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
