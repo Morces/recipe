1b7ead8c31eb52284d4d7f218e8599c2f39c56d5
Morces <karanim594@gmail.com>
2023-11-20 12:12:19 +0300

    installed tailwind

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import tw from "twrnc";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={tw`flex-1 justify-center items-center`}>
+      <Text style={tw`text-red-500`}>Home</Text>
     </View>
-  )
+  );
 }
